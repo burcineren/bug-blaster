0c9b44261f9a1fb50c33800db9457980b63cd180
burcineren <burcin.ern969@gmail.com>
2024-12-31 02:21:37 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./styles.css";
+import TicketForm from "./components/TicketForm";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bug Blaster</h1>
+        <TicketForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
